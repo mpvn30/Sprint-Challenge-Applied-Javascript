99833ceaec5f11ceaf03030489c817e98cb5bcb7
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-09-06 13:28:17 -0400

    finished cards.js

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,55 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios
+.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response => {
+    // console.log(response);
+    // console.log(response.data)
+    const data = response.data;
+    const articles = data.articles;
+    const info = Object.values(articles);
+    const addInfo = [''];
+    console.log(info.length)
+    
+    for (var i = 0; i < info.length; i++){
+        addInfo.push(info[i][i])
+        console.log(addInfo)
+    }
+    addInfo.forEach(item => {
+        const addCard = card(item);
+        enter.appendChild(addCard);
+    })
+
+})
+
+function card(obj){
+    const
+    newCard = document.createElement('div');
+    headline = document.createElement('div');
+    authorContainer = document.createElement('div');
+    imgContainer = document.createElement('div');
+    image = document.createElement('img');
+    author = document.createElement('span');
+
+    newCard.classList.add('card');
+    headline.classList.add('headline');
+    authorContainer.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    headline.textContent = obj.headline;
+    image.src = obj.authorPhoto;
+    author.textContent = obj.authorName;
+
+    newCard.appendChild(headline);
+    newCard.appendChild(authorContainer);
+    authorContainer.appendChild(imgContainer);
+    imgContainer.appendChild(image);
+    authorContainer.appendChild(author);
+
+    return newCard;
+}
+
+const enter = document.querySelector('.cards-container')
+ 
